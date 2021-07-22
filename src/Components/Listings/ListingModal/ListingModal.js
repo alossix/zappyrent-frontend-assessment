3a3c37064724ab4bfba6045098f73f65906b3555
Rich Alossi Staiy <r.alossi.staiy@gmail.com>
2021-07-22 14:02:57 +0200

    Further develop modal logic and styling

--- src/Components/Listings/ListingModal/ListingModal.js
+++ src/Components/Listings/ListingModal/ListingModal.js
@@ -1,17 +1,30 @@
 import React from 'react';
 
-import ListingDescription from '../ListingCard/ListingDescription/ListingDescription';
+import CloseButton from './close-button.png';
 
 import {
   ModalBackdrop,
   Card,
+  CloseButtonContainer,
+  CloseButtonImage,
+  ListingTitle,
   ImageContainer,
   AvailableButton,
+  Address,
+  DescriptionContainer,
+  ListingDetailsContainer,
+  ListingDetailContainer,
+  ListingDetailNumber,
+  ListingDetailText,
+  ListingDescriptionContainer,
+  ListingDescriptionText,
+  HR,
   CanoneContainer,
   CanoneTitle,
   CanonePriceContainer,
   CanonePrice,
   CanoneMese,
+  ReserveButton,
 } from './style';
 
 const ListingModal = ({
@@ -25,33 +38,69 @@ const ListingModal = ({
   };
 
   return (
-    <>
-      <ModalBackdrop onClick={clickHandler} />
+    <ModalBackdrop onClick={clickHandler}>
       <Card>
-        <ImageContainer src={modalListing.image}>
+        <CloseButtonContainer>
+          <CloseButtonImage src={CloseButton} />
+        </CloseButtonContainer>
+        <ListingTitle>{modalListing.title}</ListingTitle>
+        <ImageContainer src={modalListing.images[0].url}>
           {modalListing.available && (
             <AvailableButton>Disponibile subito</AvailableButton>
           )}
         </ImageContainer>
-        <ListingDescription
-          type={modalListing.type}
-          title={modalListing.title}
-          tenants={modalListing.tenants}
-          baths={modalListing.baths}
-          beds={modalListing.beds}
-          description={modalListing.description}
-        />
+        <DescriptionContainer>
+          <ListingDetailsContainer>
+            <ListingDetailContainer>
+              <ListingDetailNumber>
+                {modalListing.tenants}
+              </ListingDetailNumber>
+              <ListingDetailText>
+                {modalListing.tenants === 1
+                  ? 'inquilino'
+                  : 'inquilini'}
+              </ListingDetailText>
+            </ListingDetailContainer>
+            <ListingDetailContainer>
+              <ListingDetailNumber>
+                {modalListing.baths}
+              </ListingDetailNumber>
+              <ListingDetailText>
+                {modalListing.baths === 1 ? 'bagno' : 'bagni'}
+              </ListingDetailText>
+            </ListingDetailContainer>
+            <ListingDetailContainer>
+              <ListingDetailNumber>
+                {modalListing.beds}
+              </ListingDetailNumber>
+              <ListingDetailText>
+                {modalListing.beds === 1 ? 'letto' : 'letti'}
+              </ListingDetailText>
+            </ListingDetailContainer>
+          </ListingDetailsContainer>
+          <Address>
+            {modalListing.street}&nbsp;{modalListing.street_number},
+            &nbsp;{modalListing.cap}&nbsp;{modalListing.city}
+          </Address>
+          <ListingDescriptionContainer>
+            <ListingDescriptionText>
+              {modalListing.description}
+            </ListingDescriptionText>
+          </ListingDescriptionContainer>
+        </DescriptionContainer>
+        <HR />
         <CanoneContainer>
-          <CanoneTitle>Canone d’affitto</CanoneTitle>
           <CanonePriceContainer>
+            <CanoneTitle>Canone d’affitto</CanoneTitle>
             <CanonePrice>
               €&nbsp;{modalListing.price}&nbsp;
             </CanonePrice>
             <CanoneMese>/mese</CanoneMese>
           </CanonePriceContainer>
+          <ReserveButton>Prenota alloggio</ReserveButton>
         </CanoneContainer>
       </Card>
-    </>
+    </ModalBackdrop>
   );
 };
 
--- src/Components/Listings/ListingModal/style.js
+++ src/Components/Listings/ListingModal/style.js
@@ -2,37 +2,52 @@ import styled from 'styled-components';
 
 export const ModalBackdrop = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: center;
   position: fixed;
   top: 0;
   left: 0;
   width: 100vw;
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.7);
-
-  /* &.open {
-    display: flex;
-    position: fixed;
-  } */
 `;
 
 export const Card = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  width: 350px;
-  height: 410px;
+  width: 480px;
+  height: 500px;
   border-radius: 20px;
   box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
-  z-index: 100;
+  background-color: white;
+  padding: 20px 10px;
+`;
+
+export const CloseButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+  height: 20px;
+`;
+
+export const CloseButtonImage = styled.img`
+  width: 22px;
+  padding-right: 10px;
+  cursor: pointer;
+`;
+
+export const ListingTitle = styled.p`
+  font-size: 18px;
+  font-weight: 800;
+  text-align: center;
+  margin: 0;
 `;
 
 export const ImageContainer = styled.div`
   background-image: url(${(p) => p.src});
   background-size: cover;
-  border-top-left-radius: 20px;
-  border-top-right-radius: 20px;
-  height: 40%;
-  width: 100%;
+  height: 170px;
 `;
 
 export const AvailableButton = styled.button`
@@ -49,18 +64,78 @@ export const AvailableButton = styled.button`
   left: 10px;
 `;
 
-export const CanoneContainer = styled.div`
+export const Address = styled.p`
+  text-align: center;
+  font-size: 13px;
+  font-weight: 800;
+`;
+
+export const DescriptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
+
+export const ListingDetailsContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin: auto;
+  width: 60%;
+  height: 20px;
+`;
+
+export const ListingDetailContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  height: 15%;
-  padding: 0 15px;
+`;
+
+export const ListingDetailNumber = styled.p`
+  display: flex;
+  align-items: flex-end;
+  font-size: 18px;
+  font-weight: 600;
+  padding-right: 5px;
+`;
+
+export const ListingDetailText = styled.p`
+  display: flex;
+  align-items: flex-end;
+  color: gray;
+  margin: 0;
+  height: 22px;
+  font-size: 13px;
+`;
+
+export const ListingDescriptionContainer = styled.div`
+  padding: 0 25px;
+`;
+
+export const HR = styled.hr`
+  width: 100%;
+  margin: 0;
+`;
+
+export const ListingDescriptionText = styled.p`
+  color: gray;
+  font-size: 13px;
+  text-align: center;
+  margin: 0;
+`;
+
+export const CanoneContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const CanoneTitle = styled.p`
   color: #05a8a4;
-  font-size: 18px;
+  font-size: 16px;
   font-weight: bold;
+  padding-right: 8px;
+  margin: 0;
+  margin-bottom: 14px;
 `;
 
 export const CanonePriceContainer = styled.div`
@@ -68,11 +143,28 @@ export const CanonePriceContainer = styled.div`
 `;
 
 export const CanonePrice = styled.p`
-  font-size: 18px;
+  font-size: 16px;
   font-weight: bold;
+  margin: 0;
 `;
+
 export const CanoneMese = styled.p`
-  font-size: 18px;
+  font-size: 16px;
   font-weight: bold;
   color: gray;
+  margin: 0;
+`;
+
+export const ReserveButton = styled.button`
+  width: 300px;
+  height: 40px;
+  background-color: #05a8a4;
+  border: none;
+  border-radius: 25px;
+  color: white;
+  font-family: 'Nunito', sans-serif;
+  font-size: 16px;
+  font-weight: bold;
+
+  cursor: pointer;
 `;
